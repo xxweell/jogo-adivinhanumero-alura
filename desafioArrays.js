918fe6765f3efa04dc48b0215a8e8ed34ada0f6c
Wellington Nogueira <wellingtoncw7@gmail.com>
2024-01-24 19:29:23 -0300

    Create desafioArrrays

---
+++ desafioArrays.js
@@ -0,0 +1,18 @@
+// Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+
+// Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+// Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+
+// Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let listaNomes = ['Wellington', 'Mari', 'Andrea'];
+console.log(listaNomes[0]);
+
+// Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+console.log(listaNomes[1]);
+
+// Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+console.log(listaNomes[2]);
